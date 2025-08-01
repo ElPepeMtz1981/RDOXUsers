118705254ca1654992803bfa8a72d9479ac71e34
ElPepeMtz1981 <elpepemtz@gmail.com>
2025-08-01 14:16:08 -0600

    add files for gitactions

---
+++ scripts/after_install.sh
@@ -0,0 +1,25 @@
+﻿#!/bin/bash
+set -e
+
+echo "Start after_install.sh" >> /home/ubuntu/rdoxusers.log
+
+# Exporta las variables para usar el SDK correcto
+export DOTNET_ROOT=$HOME/.dotnet
+export PATH=$HOME/.dotnet:$PATH
+
+sudo chown -R ubuntu:ubuntu /home/ubuntu/rdoxuserssc
+
+chmod -R u+rwX /home/ubuntu/rdoxuserssc
+
+echo "change directory publish" >> /home/ubuntu/rdoxusers.log
+
+# 👉 Navega al código fuente
+cd /home/ubuntu/rdoxuserssc
+
+echo "start publish" >> /home/ubuntu/rdoxusers.log
+# 👉 Publica en la carpeta de artefactos
+$DOTNET_ROOT/dotnet publish RDOXLogin.csproj -c Release -o /home/ubuntu/rdoxusers
+
+echo "End publish" >> /home/ubuntu/rdoxusers.log
+
+echo "End after_install.sh." >> /home/ubuntu/rdoxusers.log
---
+++ scripts/start_server.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+set -e
+
+echo "Ejecutando start_server.sh..." >> /home/ubuntu/rdoxusers.log
+
+# Restart the api service
+sudo systemctl restart UsersService
+
+echo "Servicio reiniciado correctamente." >> /home/ubuntu/rdoxusers.log
